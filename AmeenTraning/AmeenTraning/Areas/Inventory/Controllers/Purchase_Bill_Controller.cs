79aaa5665ea1f3781b7f37f19131da8b8499b57a
Fahad Shaikh <fahad.shaikh@theperformancestencil.com>
2020-08-24 23:10:42 +0530

    purchase bill

---
+++ AmeenTraning/AmeenTraning/Areas/Inventory/Controllers/Purchase_Bill_Controller.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AmeenTraning.Models;
+
+namespace AmeenTraning.Areas.Inventory.Controllers
+{
+    public class Purchase_Bill_Controller : Controller
+    {
+        private TrainingEntities db = new TrainingEntities();
+        // GET: Inventory/Purchase_Bill_
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult IndexPartial()
+        {
+
+            return PartialView();
+        }
+        public PartialViewResult CreatePartial()
+        {
+            string Company_Id = string.Empty;
+            HttpCookie myCookie = Request.Cookies["inventoryCookie"];
+            if (!string.IsNullOrEmpty(myCookie.Values["Company_Id"]))
+            {
+                Company_Id = myCookie.Values["Company_Id"].ToString();
+            }
+            var compid = Convert.ToInt32(Company_Id);
+            ViewBag.Supplier = db.Supplier_Details.Where(e => e.Company_Id == compid).ToList();
+            return PartialView();
+        }
+    }
+}
