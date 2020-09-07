63a73089c646da772eac2e543f90083ec9cb7592
Fahad Shaikh <fahad.shaikh@theperformancestencil.com>
2020-09-07 22:22:56 +0530

    changes

--- AmeenTraning/AmeenTraning/Areas/Inventory/Controllers/Purchase_Bill_Controller.cs
+++ AmeenTraning/AmeenTraning/Areas/Inventory/Controllers/Purchase_Bill_Controller.cs
@@ -16,6 +16,14 @@ namespace AmeenTraning.Areas.Inventory.Controllers
         // GET: Inventory/Purchase_Bill_
         public ActionResult Index()
         {
+            string Company_Id = string.Empty;
+            HttpCookie myCookie = Request.Cookies["inventoryCookie"];
+            if (!string.IsNullOrEmpty(myCookie.Values["Company_Id"]))
+            {
+                Company_Id = myCookie.Values["Company_Id"].ToString();
+            }
+            var compid = Convert.ToInt32(Company_Id);
+            ViewBag.Products = db.Products_Details.Where(e => e.Company_Id == compid).ToList();
             return View();
         }
         public PartialViewResult IndexPartial()
@@ -62,11 +70,20 @@ namespace AmeenTraning.Areas.Inventory.Controllers
             }
             return PartialView(bill);
         }
-        public string Add(decimal GST, decimal GSTamount, decimal Discount, decimal GrandTotal, int Supplier_ID, decimal Quantity, decimal Transport_Charge, decimal OtherExpences, string Comment, string PaymentMode)
+        public int Add(decimal GST, decimal GSTamount, decimal Discount, decimal GrandTotal, int Supplier_ID, decimal Quantity, decimal Transport_Charge, decimal OtherExpences, string Comment, string PaymentMode)
         {
-            Purchase_Bill purchase = new Purchase_Bill();
+            string Company_Id = string.Empty;
+            HttpCookie myCookie = Request.Cookies["inventoryCookie"];
+            if (!string.IsNullOrEmpty(myCookie.Values["Company_Id"]))
+            {
+                Company_Id = myCookie.Values["Company_Id"].ToString();
+            }
+            var compid = Convert.ToInt32(Company_Id);
 
+
+            Purchase_Bill purchase = new Purchase_Bill();
             purchase.Date_Created = DateTime.Now;
+            purchase.Company_Id = compid;
             purchase.Supplier_ID = Supplier_ID;
             purchase.Quantity = Quantity;
             purchase.Discount = Discount;
@@ -79,8 +96,63 @@ namespace AmeenTraning.Areas.Inventory.Controllers
 
             db.Purchase_Bill.Add(purchase);
             db.SaveChanges();
+            var Bill_Id = db.Purchase_Bill.Where(e => e.Company_Id == compid && e.Supplier_ID == Supplier_ID && e.Total_Amount == GrandTotal).Select(e => e.Bill_No).FirstOrDefault();
+          
+            return Bill_Id;
+        }
+
+        public string Add_Item(decimal qty, decimal gst, decimal price, int prodocut_Id, decimal discount, string unit, decimal amount, int bill_No)
+        {
+            string Company_Id = string.Empty;
+            HttpCookie myCookie = Request.Cookies["inventoryCookie"];
+            if (!string.IsNullOrEmpty(myCookie.Values["Company_Id"]))
+            {
+                Company_Id = myCookie.Values["Company_Id"].ToString();
+            }
+            var compid = Convert.ToInt32(Company_Id);
+            Purchase_Bill_Item bill_Item = new Purchase_Bill_Item();
+            bill_Item.Purchase_Bill_ID = bill_No;
+            bill_Item.Product_id = prodocut_Id;
+            bill_Item.Quantity = qty;
+            bill_Item.Unit = unit;
+            bill_Item.Price = price;
+            bill_Item.Discount = discount;
+            bill_Item.Amount = amount;
+            db.Purchase_Bill_Item.Add(bill_Item);
+            db.SaveChanges();
 
             return "Bill Added";
         }
+
+        public ActionResult Delete(int id)
+        {
+            Purchase_Bill purchase_Bill = db.Purchase_Bill.Find(id);
+            db.Purchase_Bill.Remove(purchase_Bill);
+            db.SaveChanges();
+            Purchase_Bill_Item bill_Item = db.Purchase_Bill_Item.Where(e=>e.Purchase_Bill_ID == id).FirstOrDefault();
+            db.Purchase_Bill_Item.Remove(bill_Item);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult Edit(int? id)
+		{
+            string Company_Id = string.Empty;
+            HttpCookie myCookie = Request.Cookies["inventoryCookie"];
+            if (!string.IsNullOrEmpty(myCookie.Values["Company_Id"]))
+            {
+                Company_Id = myCookie.Values["Company_Id"].ToString();
+            }
+            var compid = Convert.ToInt32(Company_Id);
+            var supplier = db.Purchase_Bill.Where(e => e.Bill_No == id).FirstOrDefault();
+            var suppname = db.Supplier_Details.Where(e=>e.Supplier_Id == supplier.Supplier_ID).FirstOrDefault();
+            ViewBag.Billno = supplier.Bill_No;
+            ViewBag.Date = supplier.Date_Created;
+            ViewBag.selctedsup = suppname.Name;
+            ViewBag.Supplier = db.Supplier_Details.Where(e => e.Company_Id == compid).ToList();
+            ViewBag.Products = db.Products_Details.Where(e => e.Company_Id == compid).ToList();
+            return PartialView();
+        }
+
     }
 }
---
+++ AmeenTraning/AmeenTraning/Areas/Inventory/Controllers/Sales_BillController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AmeenTraning.Areas.Inventory.Controllers
+{
+    public class Sales_BillController : Controller
+    {
+        // GET: Inventory/Sales_Bill
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
